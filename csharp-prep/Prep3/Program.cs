f8fec0c65ab3e93d12cef02e7c4c3872856d4301
Benjamin Davila III <benjamindavilaiii@Benjamins-MacBook-Air-3.local>
2024-09-20 14:21:01 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // random number
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1,101);
+
+        int guess = 0;
+
+        // user guess
+        while (magic != guess)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }        
+        }
     }
 }
