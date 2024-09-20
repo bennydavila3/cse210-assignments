e8392d6b6ee817e8c89c8a1b1fbd969585900014
Benjamin Davila III <benjamindavilaiii@Benjamins-MacBook-Air-3.local>
2024-09-20 13:43:51 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letter = "";
+
+        // determine letter grade
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else (percent < 60)
+        {
+            letter = "F";
+        }
+
+        // print letter grade
+        Console.WriteLine(letter);
+
+        // pass or no pass
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
     }
 }
