86d5c5ec270287bd8ec7f2fc257eb1b50b57e579
Benjamin Davila III <benjamindavilaiii@Benjamins-MacBook-Air-5.local>
2024-10-18 13:47:14 -0600

    Learning 03 Complete

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello, Welcome to my Cash Register!");
+
+        Bin myBin = new("quarters", 40, (float).25);
+        myBin.ModifyAmount(6);
+        Console.WriteLine(myBin.TotalValue());
     }
 }
