b76a1f7250e5d3609ecb1da892fc232b5d073f91
Benjamin Davila III <benjamindavilaiii@Benjamins-MacBook-Air-5.local>
2024-10-31 18:56:48 -0600

    Rename Prepare 4 and 5

---
+++ prepare/Learning04/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment assignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "Problems 8-19");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+
+        WritingAssignment assignment2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingAssignment());
+    }
+}
