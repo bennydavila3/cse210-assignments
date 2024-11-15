c4d20896cefdfe1ee3981ae41505c20ae76227f9
Benjamin Davila III <benjamindavilaiii@Benjamins-MacBook-Air-5.local>
2024-11-15 12:30:49 -0700

    Learning 06 Complete

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s = new Square("Red", 3);
+        shapes.Add(s);
+
+        Rectangle r = new Rectangle("Blue", 4, 8);
+        shapes.Add(r);
+
+        Circle c = new Circle("Green", 1);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The color is {color} with an area of {area}.");
+        }
     }
 }
